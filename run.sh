85ad939018b116da863b5a6365b6eafbcc20c1df
Bruno Ribeiro da Silva <brunoqkz@gmail.com>
2019-06-08 13:56:49 -0300

    Improved test routines of run.sh

--- run.sh
+++ run.sh
@@ -1,44 +1,48 @@
-# ERROS
-mvn exec:java '-Dexec.args="src/main/javacc/code/lexico/exemplo_erro.x"' -q --log-file target/exemplo_erro_lexico.log
-# TODO
-mvn exec:java '-Dexec.args="src/main/javacc/code/sintatico/exemplo_erro.x"' -q --log-file target/exemplo_erro_sintatico.log
+#!/usr/bin/env bash
 
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/atribstat.x"'  -q --log-file target/atribstat.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/bintree-erro-lexico.x"'  -q --log-file target/bintree-erro-lexico.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/bintree-erro-sintatico.x"'  -q --log-file target/bintree-erro-sintatico.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/classbody.x"' -q --log-file target/classbody.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/classbody2.x"' -q --log-file target/classbody2.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/classdecl.x"' -q --log-file target/classdecl.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/empty.x"' -q --log-file target/empty.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/expression.x"' -q --log-file target/expression.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/matriz.x"' -q --log-file target/matriz.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/methodbody.x"' -q --log-file target/methodbody.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/methoddecl.x"' -q --log-file target/methoddecl.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/paramlist.x"' -q --log-file target/paramlist.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/program.x"' -q --log-file target/program.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/statement2.x"' -q --log-file target/statement2.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/vardecl.x"' -q --log-file target/vardecl.log
+# List of examples of syntactic and lexical corrects examples of code
+_samples=(
+    "src/main/javacc/code/lexico/exemplo.x"
+    "src/main/javacc/code/samples/SClass.x"
+    "src/main/javacc/code/samples/bintree-with-main.x"
+    "src/main/javacc/code/samples/bintree.x"
+    "src/main/javacc/code/samples/coercao.x"
+    "src/main/javacc/code/samples/debugAS.x"
+    "src/main/javacc/code/samples/statement.x"
+    "src/main/javacc/code/sintatico/exemplo.x"
+)
 
+# List of examples of syntactic or lexical errors examples of code
+_error_samples=(
+    "src/main/javacc/code/lexico/exemplo_erro.x"
+    "src/main/javacc/code/samples/atribstat.x"
+    "src/main/javacc/code/samples/bintree-erro-lexico.x"
+    "src/main/javacc/code/samples/bintree-erro-sintatico.x"
+    "src/main/javacc/code/samples/classbody.x"
+    "src/main/javacc/code/samples/classbody2.x"
+    "src/main/javacc/code/samples/classdecl.x"
+    "src/main/javacc/code/samples/empty.x"
+    "src/main/javacc/code/samples/expression.x"
+    "src/main/javacc/code/samples/matriz.x"
+    "src/main/javacc/code/samples/methodbody.x"
+    "src/main/javacc/code/samples/methoddecl.x"
+    "src/main/javacc/code/samples/paramlist.x"
+    "src/main/javacc/code/samples/program.x"
+    "src/main/javacc/code/samples/statement2.x"
+    "src/main/javacc/code/samples/vardecl.x"
+    "src/main/javacc/code/sintatico/exemplo_err.x"
+)
 
-# CORRETOS
-mvn exec:java '-Dexec.args="src/main/javacc/code/lexico/exemplo.x"' -q --log-file target/exemplo_lexico.log
-# TODO
-mvn exec:java '-Dexec.args="src/main/javacc/code/sintatico/exemplo.x"' -q --log-file target/exemplo_sintatico.log
+mvn clean compile -q
 
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/bintree-with-main.x"' -q --log-file target/bintree-with-main.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/bintree.x"' -q --log-file target/bintree.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/coercao.x"' -q --log-file target/coercao.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/debugAS.x"' -q --log-file target/debugAS.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/SClass.x"'  -q --log-file target/SClass.log
-mvn exec:java '-Dexec.args="src/main/javacc/code/samples/statement.x"' -q --log-file target/statement.log
+echo "TESTING SYNTACTIC AND LEXICAL SAMPLES"
+for sample in "${_samples[@]}"; do
+    mvn exec:java -q -Dexec.args="$sample"
+done
 
+echo "_________________________________________________"
 
-
-
-
-
-
-
-
-
-
+echo "TESTING SYNTACTIC AND LEXICAL SAMPLES WITH ERRORS"
+for error_sample in "${_error_samples[@]}"; do
+    mvn exec:java -q -Dexec.args="$error_sample"
+done
